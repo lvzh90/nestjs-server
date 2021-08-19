7381b161eac3afd40713a074dca2d761d867ab6d
Linda Vanessa Zapata Henao <linda.zapatah@perficient.com>
2021-08-18 22:15:20 -0500

    feat(app): Create file controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { FileController } from './file/file.controller';
+import { FileModule } from './file/file.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [FileModule],
+  controllers: [AppController, FileController],
   providers: [AppService],
 })
 export class AppModule {}
