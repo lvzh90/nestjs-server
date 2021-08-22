3991e188aedb520f5357304e81304ca06d1ea56e
niltron <51491500+niltron@users.noreply.github.com>
2021-08-21 20:29:42 -0500

    Fix dependencies
    Explanation: File* files should be included only on the FileModule.

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,16 +2,14 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { FileController } from './file/file.controller';
 import { FileModule } from './file/file.module';
-import { FileService } from './file/file.service';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost/files'),
     FileModule
   ],
-  controllers: [AppController, FileController],
-  providers: [AppService, FileService],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
